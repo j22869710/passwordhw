7267b07f49b9ab1432f2801a65a8d855ae4c3f5f
Jessica <amy89.2.2554@gmail.com>
2023-08-15 01:20:46 +0800

    first

---
+++ pa1.py
@@ -0,0 +1,12 @@
+x = 3
+while x > 0:
+	password = input('請輸入密碼: ')
+	if password == 'a123456':
+		print('登入成功')
+		break
+	else:
+		x = x - 1
+		if x == 0:
+			break
+		else:
+			print('密碼錯誤!', '還有', x, '次機會')
